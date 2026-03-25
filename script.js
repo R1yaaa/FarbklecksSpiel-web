eebe6a0217b053f959e9aba8548a5bc53792bcc4
Riya <170878922+R1yaaa@users.noreply.github.com>
2026-03-25 14:38:02 +0100

    create Kleckse & positions

--- script.js
+++ script.js
@@ -1,1 +1,67 @@
 const gameContainer = document.getElementById('game-container');
+const klecksBilder = [
+    "images/klecks2.png",
+    "images/klecks3.png",
+    "images/klecks4.png",
+    "images/klecks5.png"
+]
+const klecksPositionen = [];
+const minDistance = 100;
+
+
+function createKleckse() {
+    for (let i = 0; i < 30; i++) {
+        const randomIndex = Math.floor(Math.random() * klecksBilder.length);
+        const randomBild = klecksBilder[randomIndex];
+
+        const klecksGroesse = 120;
+
+        const klecks = document.createElement("div");
+        klecks.classList.add("klecks");
+        klecks.style.backgroundImage = `url("${randomBild}")`;
+        klecks.style.width = "100px";
+        klecks.style.height = "100px";
+        klecks.style.position = "absolute";
+        const position = getValidPosition(klecksGroesse);
+        klecks.style.left = position.x + "px";
+        klecks.style.top = position.y + "px";
+        klecks.style.backgroundSize = "contain";
+        klecks.style.backgroundRepeat = "no-repeat";
+        klecks.style.backgroundPosition = "center";
+
+        klecksPositionen.push({x: position.x, y: position.y});
+
+    gameContainer.appendChild(klecks);
+    }
+}
+
+function getValidPosition(klecksGroesse) {      //random x,y coordinates
+    const containerWidth = gameContainer.clientWidth;
+    const containerHeight = gameContainer.clientHeight;
+
+
+    let randomX;
+    let randomY;
+    let validPosition = false;
+
+    while (!validPosition) {
+        randomX = Math.floor(Math.random() * (containerWidth - klecksGroesse)); // damit es am Rand nicht rausragt
+        randomY = Math.floor(Math.random() * (containerHeight - klecksGroesse));
+        validPosition = true;
+
+        for (const pos of klecksPositionen) {
+            const dx = randomX - pos.x;
+            const dy = randomY - pos.y;
+            const distance = Math.sqrt(dx * dx + dy * dy); 
+
+            if(minDistance > distance) {
+                validPosition = false;
+                break;
+            }
+        }
+    }
+    return {x: randomX, y: randomY};
+}
+
+
+createKleckse();
